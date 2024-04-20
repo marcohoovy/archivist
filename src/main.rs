3f7e09c157f88c668cde1cd47a0de0f01bd1f8c6
harvey298 <marco77@live.co.uk>
2024-04-20 17:21:38 +0100

    update deps & add cli args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use std::{fs, time::{SystemTime, UNIX_EPOCH}, env};
+
 use git2::Repository;
 use serde::{Deserialize, Serialize};
 
+#[allow(unused_assignments)]
 fn main() {
-    let projects_path = "B:/etc/projects/";
-    let out_file = "./projects.toml";
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Usage: {} <work folder> <out-file> [input-file]", args[0]);
+        return;
+    }
+
+    let out_file = &args[2];
+    let projects_path = &args[1];
+
+    let mut build_config = match fs::read_to_string(args.get(3).map(String::as_str).unwrap_or("./projects.toml")) {
+        Ok(file) => { toml::from_str::<BuildConfig>(&file).unwrap() },
+        Err(_) => {
+            BuildConfig {
+                project: vec![],
+            }
+        }
+    };
+
     for entry in std::fs::read_dir(projects_path).unwrap() {
         let entry = entry.unwrap().path();
 
@@ -18,41 +39,83 @@ fn main() {
 
                 let src_status = if project.package.homepage.is_some() { SourceStatus::Open } else { SourceStatus::Closed };
 
-                println!("{entry:?}");
+                let mut last_commit_time = 0;
+                let mut first_commit_time = 0;
 
-                if let Ok(repo) = Repository::open(".") {
-                    println!("{}",repo.message().unwrap());
+                if let Ok(repo) = Repository::open(entry.clone()) {
+                    if let Ok(head) = repo.head() { 
+                        if let Ok(commit) = head.peel_to_commit() {
+
+                            if let Some(parent) = commit.parents().last() {
+                                first_commit_time = parent.time().seconds();
+                            }
+        
+                            last_commit_time = commit.time().seconds();
+                        };
+                    } else { println!("No head! Please Commit! ({entry:?})"); };
                 };
+
+                let start = SystemTime::now();
+                let current_epoch = start
+                    .duration_since(UNIX_EPOCH)
+                    .expect("Time went backwards");
+
+                let current_epoch = current_epoch.as_secs() as i64;
+
+                let time_since = current_epoch - last_commit_time;
+
+                let new = (current_epoch - first_commit_time) < 1_555_200; // 18 days
+
+                let maintance_status = if time_since > 2592000 { // 30 days
+                    if time_since > 7776000 { MaintenanceStatus::Deprecated } else { MaintenanceStatus::Maintenance } // 90 days
+                } else { MaintenanceStatus::Active };
 
                 let item = BuildConfigProject {
                     name: project.package.name,
                     description: project.package.description,
                     source_status: src_status,
                     src: project.package.homepage,
-                    dev_status: todo!(),
-                    license: todo!(),
-                    maintenance: todo!(),
-                    languages: todo!(),
-                    tools: todo!(),
+                    dev_status: DevelopmentStatus::Stable,
+                    license: None,
+                    maintenance: maintance_status,
+                    languages: vec!["rust".to_string()], // TODO: Make this dynamic
+                    tools: vec![],
+                    epoch: last_commit_time,
+                    new: new,
                 };
-            }
 
+                build_config.add_project(item);
+            }
         }
-        
     }
+
+    fs::write(out_file, toml::to_string_pretty(&build_config).unwrap()).unwrap();
+
 }
 
 /// The Config used in building the website
 #[derive(Debug, Serialize, Deserialize)]
-pub struct BuildConfig {
-    pub projects: Vec<BuildConfigProject>,
-}
+pub struct BuildConfig { pub project: Vec<BuildConfigProject> }
 
 impl BuildConfig {
-    pub fn add_project(&mut self, project: BuildConfigProject)  {
-        if self.projects.clone().into_iter().any(|p| p.name == project.name) {
-            println!("Project already exists");
+    pub fn add_project(&mut self, project: BuildConfigProject) {
+        for item in self.project.iter_mut() {
+            let p2 = project.clone();
+            if item.name.replace("-", " ") == p2.name.replace("-", " ") {
+
+                item.description = p2.description;
+                item.source_status = p2.source_status;
+                item.src = p2.src;
+                item.license = p2.license;
+                item.maintenance = p2.maintenance;
+                item.epoch = p2.epoch;
+                item.new = p2.new;
+
+                return;
+            }
         }
+
+        self.project.push(project);
     }
 }
 
@@ -64,10 +127,12 @@ pub struct BuildConfigProject {
     source_status: SourceStatus,
     src: Option<String>,
     dev_status: DevelopmentStatus,
-    license: String,
+    license: Option<String>,
     maintenance: MaintenanceStatus,
     languages: Vec<String>,
     tools: Vec<String>,
+    epoch: i64,
+    new: bool,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -76,6 +141,8 @@ pub enum SourceStatus {
     Closed,
     #[serde(rename = "open")]
     Open,
+    #[serde(rename = "planned")]
+    Planned,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -97,7 +164,6 @@ pub enum DevelopmentStatus {
     #[serde(rename = "alpha")]
     Alpha,
 }
-
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CargoProject {
